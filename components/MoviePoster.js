e7409939c5bdb43fff7db263ca05631203d5281e
niclast7611 <niclast7611@gmail.com>
2022-12-22 21:55:57 -0800

    api is connected

---
+++ components/MoviePoster.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Image from "next/image";
+import { AiFillStar, AiFillClockCircle } from "react-icons/ai";
+
+export default function MoviePoster({ movieValue }) {
+  return (
+    <div className="relative transition duration-500 transform hover:scale-105 w-[225px] hover:cursor-pointer">
+      <div className="z-0">
+        <Image
+          src={`https://image.tmdb.org/t/p/original${movieValue?.poster_path}`}
+          height={225}
+          width={225}
+          alt="movie"
+        />
+      </div>
+      <div className="absolute bottom-0 z-10 bg-black/[.65] h-1/3 w-[225px] flex flex-col items-start justify-evenly pl-2">
+        <div className="text-lg font-bold text-white">
+          {movieValue?.original_title}
+        </div>
+        <div className="z-10 flex items-end justify-between w-9/12 text-base leading-4 text-white">
+          <span className="flex flex-row leading-4">
+            <span className="text-red-600 ">
+              <AiFillStar />
+            </span>
+            {movieValue?.vote_average}
+          </span>
+          <span>HD</span>
+          <span>{movieValue?.adult ? "18+" : "16+"}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
--- components/carousels/TopRated.tsx
+++ components/carousels/TopRated.tsx
@@ -4,63 +4,27 @@ import { Navigation } from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
 import MoviePoster from "../MoviePoster";
+import { useGetTopRatedMoviesQuery } from "../../redux/routesApi";
 
-export default function Carousel() {
+export default function TopRated() {
+  const api_token = process.env.MOVIES_API_KEY;
+  const { data, isLoading } = useGetTopRatedMoviesQuery(api_token);
+
+  if (isLoading) return <div>Loading...</div>;
+
   return (
     <div className="">
       <div className="pl-4 ml-4 text-2xl text-white border-l-[3px] border-l-red-600 font-bold uppercase">
-        Carousel Title
+        Top Rated Movies
       </div>
       <Swiper slidesPerView={6} navigation modules={[Navigation]}>
-        <SwiperSlide>
-          <MoviePoster />
-        </SwiperSlide>
-        <SwiperSlide>
-          <MoviePoster />
-        </SwiperSlide>
-        <SwiperSlide>
-          <MoviePoster />
-        </SwiperSlide>
-        <SwiperSlide>
-          <MoviePoster />
-        </SwiperSlide>
-        <SwiperSlide>
-          <MoviePoster />
-        </SwiperSlide>
-        <SwiperSlide>
-          <MoviePoster />
-        </SwiperSlide>
-        <SwiperSlide>
-          <MoviePoster />
-        </SwiperSlide>
-        <SwiperSlide>
-          <MoviePoster />
-        </SwiperSlide>
-        <SwiperSlide>
-          <MoviePoster />
-        </SwiperSlide>
-        <SwiperSlide>
-          <MoviePoster />
-        </SwiperSlide>
-        <SwiperSlide>
-          <MoviePoster />
-        </SwiperSlide>
-        <SwiperSlide>
-          <MoviePoster />
-        </SwiperSlide>
-        <SwiperSlide>
-          <MoviePoster />
-        </SwiperSlide>
-        <SwiperSlide>
-          <MoviePoster />
-        </SwiperSlide>
-        <SwiperSlide>
-          <MoviePoster />
-        </SwiperSlide>
-        <SwiperSlide>
-          <MoviePoster />
-        </SwiperSlide>
+        {data.results.map((value) => (
+          <SwiperSlide key={value.id}>
+            <MoviePoster movieValue={value} />
+          </SwiperSlide>
+        ))}
       </Swiper>
+      ;
     </div>
   );
 }
