e9bd259dbe5650f42891accbcf8c9d06e2dc1909
niclast7611 <niclast7611@gmail.com>
2022-12-28 15:21:02 -0800

    some styling is done

--- components/MoviePage.js
+++ components/MoviePage.js
@@ -1,9 +1,13 @@
 import React from "react";
-import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+import { AiOutlineHeart, AiFillHeart, AiFillStar } from "react-icons/ai";
+import IconButton from "@mui/material/IconButton";
+import Rating from "@mui/material/Rating";
+
 function MoviePage({ movieInfo }) {
-  console.log(movieInfo);
+  const releaseYear = movieInfo.release_date.slice(0, 4);
+
   return (
-    <div className="flex flex-row justify-evenly">
+    <div className="flex flex-row text-white justify-evenly bg-neutral-900">
       <div>
         <img
           src={`https://image.tmdb.org/t/p/original${movieInfo?.poster_path}`}
@@ -12,44 +16,69 @@ function MoviePage({ movieInfo }) {
           height="auto"
         />
       </div>
-      <div>
-        <AiOutlineHeart />
-        <AiFillHeart />
-        <div>{movieInfo.title}</div>
-        <div>
-          <div>{movieInfo.vote_average}</div>
-          <div>{movieInfo.budget}</div>
-          <div>
+      <div className="w-5/12">
+        <div className="flex flex-row">
+          <div className="pr-3 text-4xl uppercase">{movieInfo.title}</div>
+          <div className="my-auto text-2xl">
+            {/* <IconButton
+              color="primary"
+              aria-label="upload picture"
+              component="label"
+            ></IconButton> */}
+            {/* <AiOutlineHeart style={{ color: "red" }} /> */}
+            <AiFillHeart style={{ color: "red" }} />
+          </div>
+        </div>
+        <div className="flex flex-row justify-between">
+          <div className="flex flex-row">
+            <Rating
+              name="read-only"
+              value={Math.round(movieInfo.vote_average)}
+              readOnly
+              max={10}
+              emptyIcon={
+                <AiFillStar
+                  style={{ color: "white", opacity: 0.55 }}
+                  fontSize="inherit"
+                />
+              }
+            />
+            <div className="pl-2 text-xl leading-2">
+              {Math.round(movieInfo.vote_average)}
+            </div>
+          </div>
+          <div className="text-lg tracking-wider text-yellow-400 uppercase ">
             {movieInfo.original_language}/{movieInfo.runtime}min/
-            {movieInfo.release_date}
+            {releaseYear}
           </div>
-          <div>
-            <span>The Genres</span>
-            <div>
-              {movieInfo.genres.map((genre) => (
-                <span key={genre.id}>{genre.name}</span>
-              ))}
-            </div>
+        </div>
+        {/* <div>{movieInfo.budget}</div> */}
+        <div>
+          <span className="font-bold uppercase ">The Genres</span>
+          <div className="space-x-5 text-sm text-red-600 uppercase">
+            {movieInfo.genres.map((genre) => (
+              <span key={genre.id}>{genre.name}</span>
+            ))}
           </div>
+        </div>
+        <div>
+          <span className="font-bold uppercase">Overview</span>
+          <div className="">{movieInfo.overview}</div>
+        </div>
+        <div>
+          <span>Product Companies</span>
           <div>
-            <span>Overview</span>
-            <div>{movieInfo.overview}</div>
-          </div>
-          <div>
-            <span>Product Companies</span>
-            <div>
-              {movieInfo.production_companies.map((pc) => (
-                <div key={pc.id}>
-                  <img
-                    src={`https://image.tmdb.org/t/p/original${pc.logo_path}`}
-                    alt="movie"
-                    width="100"
-                    height="auto"
-                  />
-                  <span>{pc.name}</span>
-                </div>
-              ))}
-            </div>
+            {movieInfo.production_companies.map((pc) => (
+              <div key={pc.id}>
+                <img
+                  src={`https://image.tmdb.org/t/p/original${pc.logo_path}`}
+                  alt="movie"
+                  width="100"
+                  height="auto"
+                />
+                <span>{pc.name}</span>
+              </div>
+            ))}
           </div>
         </div>
       </div>
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 export default function NavBar() {
   return (
     <div className="flex w-full h-20 bg-black">
       <div className="my-auto ml-5 text-4xl text-white">
-        Movies<span className="pl-1 text-5xl text-red-600">+</span>
+        <Link href={"/"}>
+          Movies<span className="pl-1 text-5xl text-red-600">+</span>
+        </Link>
       </div>
     </div>
   );
