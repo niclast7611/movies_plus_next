3b740488774c4148560b99a3bb1e80aee5210f8b
niclast7611 <niclast7611@gmail.com>
2022-12-28 17:19:08 -0800

    pretty much styled adding favorites option

--- components/MoviePage.js
+++ components/MoviePage.js
@@ -2,31 +2,42 @@ import React from "react";
 import { AiOutlineHeart, AiFillHeart, AiFillStar } from "react-icons/ai";
 import IconButton from "@mui/material/IconButton";
 import Rating from "@mui/material/Rating";
+import { formatter } from "../utils/helpers";
 
 function MoviePage({ movieInfo }) {
   const releaseYear = movieInfo.release_date.slice(0, 4);
+  console.log(movieInfo);
 
   return (
-    <div className="flex flex-row text-white justify-evenly bg-neutral-900">
+    <div className="flex flex-row justify-around py-10 text-white bg-neutral-900">
       <div>
         <img
           src={`https://image.tmdb.org/t/p/original${movieInfo?.poster_path}`}
           alt="movie"
-          width="300"
+          width="375"
           height="auto"
         />
       </div>
-      <div className="w-5/12">
-        <div className="flex flex-row">
-          <div className="pr-3 text-4xl uppercase">{movieInfo.title}</div>
-          <div className="my-auto text-2xl">
-            {/* <IconButton
+      <div className="flex flex-col w-6/12 space-y-6">
+        <div className="flex flex-row justify-between">
+          <div className="flex flex-row">
+            <div className="pb-2 pr-3 text-5xl uppercase">
+              {movieInfo.title}
+            </div>
+
+            <div className="my-auto text-2xl">
+              {/* <IconButton
               color="primary"
               aria-label="upload picture"
               component="label"
             ></IconButton> */}
-            {/* <AiOutlineHeart style={{ color: "red" }} /> */}
-            <AiFillHeart style={{ color: "red" }} />
+              {/* <AiOutlineHeart style={{ color: "red" }} /> */}
+              {/* <AiFillHeart style={{ color: "red" }} /> */}
+            </div>
+          </div>
+          <div className="my-auto text-xl tracking-wider text-yellow-400 uppercase">
+            {movieInfo.original_language}/{movieInfo.runtime}min/
+            {releaseYear}
           </div>
         </div>
         <div className="flex flex-row justify-between">
@@ -36,6 +47,7 @@ function MoviePage({ movieInfo }) {
               value={Math.round(movieInfo.vote_average)}
               readOnly
               max={10}
+              size="large"
               emptyIcon={
                 <AiFillStar
                   style={{ color: "white", opacity: 0.55 }}
@@ -47,39 +59,33 @@ function MoviePage({ movieInfo }) {
               {Math.round(movieInfo.vote_average)}
             </div>
           </div>
-          <div className="text-lg tracking-wider text-yellow-400 uppercase ">
-            {movieInfo.original_language}/{movieInfo.runtime}min/
-            {releaseYear}
-          </div>
         </div>
         {/* <div>{movieInfo.budget}</div> */}
         <div>
           <span className="font-bold uppercase ">The Genres</span>
-          <div className="space-x-5 text-sm text-red-600 uppercase">
+          <div className="pt-2 space-x-5 text-sm text-red-600 uppercase">
             {movieInfo.genres.map((genre) => (
               <span key={genre.id}>{genre.name}</span>
             ))}
           </div>
         </div>
         <div>
-          <span className="font-bold uppercase">Overview</span>
-          <div className="">{movieInfo.overview}</div>
-        </div>
-        <div>
-          <span>Product Companies</span>
-          <div>
-            {movieInfo.production_companies.map((pc) => (
-              <div key={pc.id}>
-                <img
-                  src={`https://image.tmdb.org/t/p/original${pc.logo_path}`}
-                  alt="movie"
-                  width="100"
-                  height="auto"
-                />
-                <span>{pc.name}</span>
-              </div>
-            ))}
+          <span className="font-bold uppercase ">Overview</span>
+          <div className="pt-2 leading-7 text-neutral-400">
+            {movieInfo.overview}
           </div>
+        </div>
+        <div className="flex flex-col">
+          <div className="pb-2 font-bold uppercase">tagline</div>
+          <span className="text-neutral-400">
+            &quot; {movieInfo.tagline} &quot;
+          </span>
+        </div>
+        <div className="flex flex-col">
+          <div className="pb-2 font-bold uppercase">budget</div>
+          <span className="text-neutral-400">
+            {formatter.format(movieInfo.budget)}
+          </span>
         </div>
       </div>
     </div>
